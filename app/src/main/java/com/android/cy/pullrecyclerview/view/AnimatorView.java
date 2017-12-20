b928d75b5c08f287759b839e337fb8e38784cefa
zhxh <zhuxiaohui@niuguwang.com>
2017-12-20 10:22:10 +0800

    Initial commit

---
+++ app/src/main/java/com/android/cy/pullrecyclerview/view/AnimatorView.java
@@ -0,0 +1,96 @@
+package com.android.cy.pullrecyclerview.view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.android.cy.pullrecyclerview.R;
+
+public class AnimatorView extends RelativeLayout {
+
+    private View mView;
+    private LinearLayout mLayout;
+    private TextView mTextView;
+    private ImageView iv_arrow;
+    private int mMove;
+    private Path mPath;
+    private Paint mBackPaint;
+    private int mHeight;
+    private int mLayoutHeight;
+    private int mLayoutWidth;
+
+    public AnimatorView(Context context) {
+        super(context);
+        mPath = new Path();
+        mBackPaint = new Paint();
+        mBackPaint.setAntiAlias(true);
+        mBackPaint.setStyle(Paint.Style.FILL);
+        mBackPaint.setColor(0xffF3F3F3);
+
+        mView = View.inflate(context, R.layout.animator_footer, null);
+        mLayout = (LinearLayout) mView.findViewById(R.id.animator_ll);
+        mTextView = (TextView) mView.findViewById(R.id.animator_text);
+        iv_arrow = (ImageView) mView.findViewById(R.id.iv_arrow);
+        addView(mView);
+    }
+
+    public AnimatorView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public AnimatorView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+        mHeight = getHeight();
+        mLayoutHeight = mLayout.getHeight();
+        mLayoutWidth = mLayout.getWidth();
+    }
+
+    public void setRefresh(int width) {
+        mMove += width;
+        if (mMove < 0) {
+            mMove = 0;
+        } else if (mMove > CYStickyNavLayouts.maxWidth) {
+            mMove = CYStickyNavLayouts.maxWidth;
+        }
+        mView.getLayoutParams().width = mMove;
+        mView.getLayoutParams().height = LinearLayout.LayoutParams.MATCH_PARENT;
+
+        if (mMove > CYStickyNavLayouts.maxWidth / 2) {
+            mTextView.setText("释放查看");
+            iv_arrow.setImageResource(R.drawable.tactics_more_left);
+        } else {
+            mTextView.setText("查看更多");
+            iv_arrow.setImageResource(R.drawable.tactics_more_right);
+        }
+        requestLayout();
+    }
+
+    public void setRelease() {
+        mMove = 0;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        mPath.reset();
+        float marginTop = (mHeight - mLayoutHeight) / 2;
+        // 右上角x坐标、右上角y坐标
+        mPath.moveTo(mMove - mLayoutWidth, marginTop);
+        // 左边弧形x坐标、左边弧形y坐标、右下角x坐标、右下角y坐标
+        mPath.quadTo(0, mHeight / 2, mMove - mLayoutWidth, mLayoutHeight + marginTop);
+        canvas.drawPath(mPath, mBackPaint);
+    }
+}
